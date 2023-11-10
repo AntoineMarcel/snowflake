ac42b4e1838bab7ff38f07cd131412baac747fe6
Antoine MARCEL <33255493+AntoineMarcel@users.noreply.github.com>
2023-11-10 13:34:42 +0100

    Update DeprecationNotice.js

--- components/DeprecationNotice.js
+++ components/DeprecationNotice.js
@@ -1,22 +1,22 @@
 export default () => (
   <div>
     <style jsx>{`
-    div {
-      background: #fdd;
-      margin: 0 auto;
-      padding: 4px 20px;
-      max-width: 800px;
-      border-radius: 4px;
-      color: #600;
-    }
-    a {
-      color: blue;
-      text-decoration: underline;
-    }
-  `}</style>
-    <h2>⚠️ Heads up: Medium isn’t using this tool anymore, but you’re welcome to!</h2>
+      div {
+        background: #0077b6; // Couleur bleu océan
+        margin: 0 auto;
+        padding: 4px 20px;
+        max-width: 800px;
+        border-radius: 4px;
+        color: white; // Texte en blanc pour contraster avec le fond bleu
+      }
+      a {
+        color: #ade8f4; // Une nuance plus claire de bleu pour les liens
+        text-decoration: underline;
+      }
+    `}</style>
+    <h2>👋 Bienvenue à tous les utilisateurs!</h2>
     <p>
-      <a href="https://medium.engineering/engineering-growth-at-medium-4935b3234d25">Read more about our current thinking around engineer growth</a>.
+      <a href="https://medium.engineering/engineering-growth-at-medium-4935b3234d25">En savoir plus sur notre approche actuelle de la croissance des ingénieurs</a>.
     </p>
   </div>
 )
